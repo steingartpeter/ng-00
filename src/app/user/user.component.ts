d8032ea91ff8cd255370e56fcd3799fd8390d36f
steingartpeter <steingart.p2@gmail.com>
2025-05-27 11:08:59 +0200

    Output 0.0

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 //import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -8,8 +15,11 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
   // SIGNAL VERSION
   // avatar = input.required<string>();
   // name = input.required<string>();
@@ -20,5 +30,6 @@ export class UserComponent {
   }
   onSelectUser() {
     console.log('User clicked...');
+    this.select.emit(this.id);
   }
 }
