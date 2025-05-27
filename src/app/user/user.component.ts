f44540b4566c15880483893510d8fc7a8838567c
steingartpeter <steingart.p2@gmail.com>
2025-05-27 09:27:19 +0200

    Siganls 0.0

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  //selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+
+  onSelectUser() {
+    console.log('User clicked...');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
