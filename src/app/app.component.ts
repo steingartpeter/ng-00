a74826ff321e6cd69b5a18c3bb8dc437c6eda180
steingartpeter <steingart.p2@gmail.com>
2025-05-28 11:52:08 +0200

    Conditional ouput with @if/@else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
   title = 'fst-ng-app';
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((usr) => usr.id === this.selectedUserId)!;
