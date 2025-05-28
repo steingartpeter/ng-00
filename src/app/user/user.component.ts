cd0ecd0fad721919406c47d7d2fdc84bed6cc493
steingartpeter <steingart.p2@gmail.com>
2025-05-28 11:33:59 +0200

    Use Object as @Input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,9 +16,14 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
   @Output() select = new EventEmitter<string>();
 
   // SIGNAL VERSION
@@ -30,10 +35,10 @@ export class UserComponent {
   // select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
     console.log('User clicked...');
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
