56e31c664a37af57bae06a5ae1a9e2e0e162ecf2
steingartpeter <steingart.p2@gmail.com>
2025-05-30 11:03:51 +0200

    Dynamic CSS class usage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -29,6 +29,7 @@ export class UserComponent {
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   // SIGNAL VERSION
