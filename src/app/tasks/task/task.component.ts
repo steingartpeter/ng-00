a004fedfc3a1fff33da1b6fab9f6340facb0dd3e
steingartpeter <steingart.p2@gmail.com>
2025-06-04 15:12:42 +0200

    Firts Piep => date, + Poppins added

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { DatePipe } from '@angular/common';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
