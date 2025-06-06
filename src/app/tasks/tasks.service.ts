e962b7219a77d08b3590c826536cc3a6fc1a1d13
steingartpeter <steingart.p2@gmail.com>
2025-06-06 12:16:05 +0200

    Create the first SERVICE

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,47 @@
+import { NewTaskData } from './task/task.model';
+
+class TasksService {
+  private tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ];
+
+  getUserTasks(usrId: string) {
+    return this.tasks.filter((tsk) => tsk.userId === usrId);
+  }
+
+  addTask(taskData: NewTaskData, usrId: string) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: usrId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((tsk) => tsk.id !== id);
+  }
+}
